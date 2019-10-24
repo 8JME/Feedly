1b86075822110a8ced9d600f549bc253994aeed5
J. Moreno <jmoreno@regis.edu>
2019-10-23 23:02:08 -0700

    config changes to pubspec, android, and ios. create page updated.

--- lib/pages/create.dart
+++ lib/pages/create.dart
@@ -1,9 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class CreatePage extends StatelessWidget {
+class CreatePage extends StatefulWidget {
+  @override
+  _CreatePageState createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  TextEditingController _postTextController = TextEditingController(text: '');
+
+  GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+  Firestore _firestore = Firestore.instance;
+  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  String user_uid, user_display_name;
+
+  _post() async {
+    if (_postTextController.text.trim().length == 0) {
+      _key.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Please enter some text to post.'),
+        ),
+      );
+
+      return;
+    }
+
+    try {
+      await _firestore.collection('posts').add({
+        'text': _postTextController.text.trim(),
+        'owner_name': user_display_name,
+        'owner': user_uid,
+        'created': DateTime.now(),
+        'likes': {},
+        'likes_count': 0,
+        'comments_count': 0,
+      });
+
+      _key.currentState.showSnackBar(SnackBar(
+        content: Text('Post created successfully.'),
+      ));
+
+      /// Allows the user to see snackbar before navigating to feed page
+      Future.delayed(
+        Duration(seconds: 1),
+        () {
+          Navigator.pop(context);
+        },
+      );
+    } catch (e) {
+      print('Exception Thrown: $e');
+      _key.currentState.showSnackBar(SnackBar(
+        content: Text(e.toString()),
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _firebaseAuth.currentUser().then((FirebaseUser user) {
+      user_uid = user.uid;
+      user_display_name = user.displayName;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _key,
       appBar: AppBar(
         title: Text('Compose New Post'),
       ),
@@ -18,6 +85,7 @@ class CreatePage extends StatelessWidget {
                 color: Colors.red,
               )),
               child: TextField(
+                controller: _postTextController,
                 maxLines: 5,
                 maxLength: 300,
                 decoration: InputDecoration(
@@ -69,7 +137,9 @@ class CreatePage extends StatelessWidget {
                       ),
                       splashColor: Colors.red,
                       color: Colors.red,
-                      onPressed: () {},
+                      onPressed: () {
+                        _post();
+                      },
                       child: Row(
                         children: <Widget>[
                           Padding(
